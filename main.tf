9c362b401c1dd1773e04f6ac208b5e4824f91e7c
pranathi <pranathi0906@gmail.com>
2023-06-12 12:17:41 +0100

    updated file

---
+++ main.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "test-vpc" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
+
+variable "public_subnet_cidrs" {
+ type        = list(string)
+ description = "Public Subnet CIDR values"
+ default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+ 
+variable "private_subnet_cidrs" {
+ type        = list(string)
+ description = "Private Subnet CIDR values"
+ default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.test-vpc.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.test-vpc.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.test-vpc.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+
+
---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "bucket_name" {
+  default = "test-demo12345-s3"
+}
+
+variable "acl_value" {
+
+  default = "private"
+
+}
+
+variable "aws_access_key" {
+
+  default = "AKIA3JN5MH3SQ26MVZ54"
+
+}
+
+variable "aws_secret_key" {
+
+  default = "C6H6Ny0BEjZhhQ6EWLt2C40F7igsqQP+U9pesDqj"
+
+}
+
+variable "region" {
+
+  default = "eu-west-2"
+
+}
